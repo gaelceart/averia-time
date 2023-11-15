86c86acccd2b52539d340e3fbcfeb0d689356a3f
Gastón <devceart@gmail.com>
2023-11-15 19:43:58 -0300

    FIX duplicates bug

--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
 import express from 'express';
+import { done_req } from './js/list';
+import {readIndex, createRow, containsName} from './utils'
 const PORT: number = +(process.env.PORT || 8081);
-const NODE_ENV = process.env.NODE_ENV ?? "development";
 
 const app = express();
-
+const dom = readIndex();
+export const document = dom.window.document;
+const tbody = document.querySelector("tbody");
 app.use(express.static("./"));
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/html/index.html");
+app.get("/", async(req,res) => {
+    const card_list = await done_req();
+    tbody.insertAdjacentHTML('beforeend', await joinRows(card_list));
+    res.send(dom.serialize())
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
+
+async function joinRows(card_list){
+    let content = ``;
+    card_list.forEach(card => {
+        const exists = containsName(card.name);
+        exists? console.log("EXISTE") : content += createRow(card.name, card.asignee, card.date, card.time)
+    });
+    return content;
+}
---
+++ utils.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import {document} from './app'
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+export function readIndex() {
+    const html = fs.readFileSync("./html/index.html");
+    const dom = new JSDOM(html);
+    return dom;
+}
+
+export function createRow(name, asignee, date, time){
+    return `
+    <tr>
+      <td>${name}</td>
+      <td>${asignee}</td>
+      <td>${date}</td>
+      <td>${time}</td>
+    </tr>
+    `;
+}
+
+export function containsName(name) {
+    const rows = document.querySelectorAll("tr");
+    for (const row of rows){
+       const cells = row.querySelectorAll('td');
+       for (const cell of cells) {
+        if (cell.textContent === name) {
+            return true;
+        }
+       }
+    };
+    return false;
+};
